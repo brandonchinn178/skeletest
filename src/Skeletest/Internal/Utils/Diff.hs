ad2b6f27f0456091e16541061e3fd55a7fa41028
Brandon Chinn <brandonchinn178@gmail.com>
2024-12-21 12:50:32 -0800

    Support Diff-1.0

--- src/Skeletest/Internal/Utils/Diff.hs
+++ src/Skeletest/Internal/Utils/Diff.hs
@@ -2,7 +2,8 @@ module Skeletest.Internal.Utils.Diff (
   showLineDiff,
 ) where
 
-import Data.Algorithm.DiffContext (getContextDiffNew, prettyContextDiff)
+import Data.Algorithm.DiffContext (getContextDiff, prettyContextDiff)
+import Data.Algorithm.DiffContext qualified as Diff
 import Data.Text (Text)
 import Data.Text qualified as Text
 import Text.PrettyPrint qualified as PP
@@ -10,7 +11,7 @@ import Text.PrettyPrint qualified as PP
 showLineDiff :: (Text, Text) -> (Text, Text) -> Text
 showLineDiff (fromName, fromContent) (toName, toContent) =
   Text.pack . PP.render $
-    prettyContextDiff (ppText fromName) (ppText toName) ppText $
-      getContextDiffNew (Just 5) (Text.lines fromContent) (Text.lines toContent)
+    prettyContextDiff (ppText fromName) (ppText toName) (ppText . Diff.unnumber) $
+      getContextDiff (Just 5) (Text.lines fromContent) (Text.lines toContent)
   where
     ppText = PP.text . Text.unpack

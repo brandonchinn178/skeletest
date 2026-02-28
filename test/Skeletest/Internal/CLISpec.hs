0e8f2b41eb0bfdb86cb9608bc8e3ba865f542fb9
Brandon Chinn <brandonchinn178@gmail.com>
2026-02-28 00:29:22 -0800

    Add CLI parsing tests

--- test/Skeletest/Internal/CLISpec.hs
+++ test/Skeletest/Internal/CLISpec.hs
@@ -1,14 +1,14 @@
 {-# LANGUAGE OverloadedRecordDot #-}
+{-# LANGUAGE OverloadedStrings #-}
 
 module Skeletest.Internal.CLISpec (spec) where
 
 import Control.Monad ((>=>))
 import Data.Dynamic (fromDynamic)
 import Data.Map qualified as Map
-import Data.Typeable (Typeable, typeOf)
+import Data.Typeable (typeOf)
 import Skeletest
 import Skeletest.Internal.CLI (
-  CLIFlagStore,
   CLIParseResult (..),
   flag,
   parseCliArgs,
@@ -16,32 +16,135 @@ import Skeletest.Internal.CLI (
 import Skeletest.Predicate qualified as P
 import Skeletest.TestUtils.Integration
 
-newtype FooFlag = FooFlag String
+newtype ToggleFlag = ToggleFlag Bool
   deriving (Eq)
-instance IsFlag FooFlag where
-  flagName = "foo"
+instance IsFlag ToggleFlag where
+  flagName = "toggle"
+  flagHelp = "test"
+  flagSpec =
+    SwitchFlag
+      { flagFromBool = ToggleFlag
+      }
+
+newtype ReqFlag = ReqFlag String
+  deriving (Eq)
+instance IsFlag ReqFlag where
+  flagName = "req"
+  flagHelp = "test"
+  flagSpec =
+    RequiredFlag
+      { flagParse = Right . ReqFlag
+      }
+
+newtype OptFlag = OptFlag String
+  deriving (Eq)
+instance IsFlag OptFlag where
+  flagName = "opt"
+  flagShort = Just 'o'
   flagHelp = "test"
   flagSpec =
     OptionalFlag
-      { flagDefault = FooFlag ""
-      , flagParse = Right . FooFlag
+      { flagDefault = OptFlag ""
+      , flagParse = Right . OptFlag
       }
 
 spec :: Spec
 spec = do
+  spec_parseCliArgs
+  spec_getFlag
+
+spec_parseCliArgs :: Spec
+spec_parseCliArgs = do
   describe "parseCliArgs" $ do
-    it "parses long flag" $ do
-      parseCliArgs [flag @FooFlag] ["--foo", "1"]
-        `shouldSatisfy` P.con CLIParseSuccess{flagStore = containsFlag (FooFlag "1")}
+    longFlagSpec
+    shortFlagSpec
+    optFlagSpec
+    reqFlagSpec
+    switchFlagSpec
+ where
+  longFlagSpec = do
+    describe "long flag" $ do
+      it "parses" $ do
+        parseCliArgs [flag @OptFlag] ["--opt", "1"]
+          `shouldSatisfy` P.con CLIParseSuccess{flagStore = containsFlag (OptFlag "1")}
 
-    it "parses long flag with equal sign" $ do
-      parseCliArgs [flag @FooFlag] ["--foo=1"]
-        `shouldSatisfy` P.con CLIParseSuccess{flagStore = containsFlag (FooFlag "1")}
+      it "parses with equal sign" $ do
+        parseCliArgs [flag @OptFlag] ["--opt=1"]
+          `shouldSatisfy` P.con CLIParseSuccess{flagStore = containsFlag (OptFlag "1")}
 
-    it "parses long flag containing equal sign" $ do
-      parseCliArgs [flag @FooFlag] ["--foo=1=2"]
-        `shouldSatisfy` P.con CLIParseSuccess{flagStore = containsFlag (FooFlag "1=2")}
+      it "parses argument containing equal sign" $ do
+        parseCliArgs [flag @OptFlag] ["--opt=1=2"]
+          `shouldSatisfy` P.con CLIParseSuccess{flagStore = containsFlag (OptFlag "1=2")}
 
+      it "errors if unknown" $ do
+        parseCliArgs [] ["--foo"]
+          `shouldSatisfy` parseFailure "Unknown flag: --foo"
+
+  shortFlagSpec = do
+    describe "short flag" $ do
+      it "parses" $ do
+        parseCliArgs [flag @OptFlag] ["-o", "123"]
+          `shouldSatisfy` P.con CLIParseSuccess{flagStore = containsFlag (OptFlag "123")}
+
+      it "errors if argument has multiple characters" $ do
+        parseCliArgs [flag @OptFlag] ["-oasdf"]
+          `shouldSatisfy` parseFailure "Invalid flag: -oasdf"
+
+      it "errors if unknown" $ do
+        parseCliArgs [] ["-x"]
+          `shouldSatisfy` parseFailure "Unknown flag: -x"
+
+  optFlagSpec = do
+    describe "OptionalFlag" $ do
+      it "stores last flag" $ do
+        parseCliArgs [flag @OptFlag] ["--opt", "1", "--opt", "2"]
+          `shouldSatisfy` P.con CLIParseSuccess{flagStore = containsFlag (OptFlag "2")}
+
+      it "stores default if not set" $ do
+        parseCliArgs [flag @OptFlag] []
+          `shouldSatisfy` P.con CLIParseSuccess{flagStore = containsFlag (OptFlag "")}
+
+      it "errors if no argument" $ do
+        parseCliArgs [flag @OptFlag] ["--opt"]
+          `shouldSatisfy` parseFailure "Flag requires argument: --opt"
+
+  reqFlagSpec = do
+    describe "RequiredFlag" $ do
+      it "stores last flag" $ do
+        parseCliArgs [flag @ReqFlag] ["--req", "1", "--req", "2"]
+          `shouldSatisfy` P.con CLIParseSuccess{flagStore = containsFlag (ReqFlag "2")}
+
+      it "errors if not set" $ do
+        parseCliArgs [flag @ReqFlag] []
+          `shouldSatisfy` parseFailure "Required flag not set: --req"
+
+      it "errors if no argument" $ do
+        parseCliArgs [flag @ReqFlag] ["--req"]
+          `shouldSatisfy` parseFailure "Flag requires argument: --req"
+
+  switchFlagSpec = do
+    describe "SwitchFlag" $ do
+      it "stores True if set" $ do
+        parseCliArgs [flag @ToggleFlag] ["--toggle"]
+          `shouldSatisfy` P.con CLIParseSuccess{flagStore = containsFlag (ToggleFlag True)}
+
+      it "stores True if set any number of times" $ do
+        parseCliArgs [flag @ToggleFlag] ["--toggle", "--toggle", "--toggle"]
+          `shouldSatisfy` P.con CLIParseSuccess{flagStore = containsFlag (ToggleFlag True)}
+
+      it "stores False if not set" $ do
+        parseCliArgs [flag @ToggleFlag] []
+          `shouldSatisfy` P.con CLIParseSuccess{flagStore = containsFlag (ToggleFlag False)}
+
+      xfail "broken" . it "errors if argument is set" $ do
+        parseCliArgs [flag @ToggleFlag] ["--toggle=asdf"]
+          `shouldSatisfy` parseFailure "Flag '--toggle' does not take arguments, got: asdf"
+
+  containsFlag f = (Map.lookup (typeOf f) >=> fromDynamic) P.>>> P.just (P.eq f)
+  parseFailure msg = P.con (CLIParseFailure (P.eq msg))
+
+spec_getFlag :: Spec
+spec_getFlag = do
   describe "getFlag" $ do
     integration . it "reads registered flag" $ do
       runner <- getFixture @TestRunner
@@ -89,6 +192,3 @@ spec = do
       (stdout, stderr) <- expectFailure runner.runTests
       stderr `shouldBe` ""
       stdout `shouldSatisfy` P.matchesSnapshot
-
-containsFlag :: (Typeable a, Eq a) => a -> Predicate IO CLIFlagStore
-containsFlag f = (Map.lookup (typeOf f) >=> fromDynamic) P.>>> P.just (P.eq f)
